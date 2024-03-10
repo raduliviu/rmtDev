0476ebf17c65bde5e996a28ec9a775e409a9bfd8
Radu Baltag <baltag.radu@gmail.com>
2024-03-10 18:03:50 +0100

    refactor useJobItem w ReactQuery

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,43 +1,39 @@
 import { useState, useEffect } from 'react';
 import { JobItem, JobItemExpanded } from './types';
 import { BASE_API_URL } from './constants';
+import { useQuery } from '@tanstack/react-query';
 
-export function useActiveId() {
-  const [activeId, setActiveId] = useState<number | null>(null);
+type JobItemApiResponse = {
+  public: boolean;
+  jobItem: JobItemExpanded;
+};
 
-  useEffect(() => {
-    const handleHashChange = () => {
-      const id = +window.location.hash.slice(1);
-      setActiveId(id);
-    };
-    handleHashChange();
-
-    window.addEventListener('hashchange', handleHashChange);
-
-    return () => {
-      window.removeEventListener('hashchange', handleHashChange);
-    };
-  }, []);
-  return activeId;
-}
+const fetchJobItem = async (id: number): Promise<JobItemApiResponse> => {
+  const response = await fetch(`${BASE_API_URL}/${id}`);
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.description);
+  }
+  const data = await response.json();
+  return data;
+};
 
 export function useJobItem(id: number | null) {
-  const [jobItem, setJobItem] = useState<JobItemExpanded | null>(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    if (!id) return;
-
-    const fetchData = async () => {
-      setIsLoading(true);
-      const response = await fetch(`${BASE_API_URL}/${id}`);
-      const data = await response.json();
-      setJobItem(data.jobItem);
-      setIsLoading(false);
-    };
-
-    fetchData();
-  }, [id]);
+  const { data, isInitialLoading } = useQuery(
+    ['job-item', id],
+    () => (id ? fetchJobItem(id) : null),
+    {
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      retry: false,
+      enabled: Boolean(id),
+      onError: (err) => {
+        console.log(err);
+      },
+    }
+  );
+  const jobItem = data?.jobItem;
+  const isLoading = isInitialLoading;
   return { jobItem, isLoading };
 }
 
@@ -75,4 +71,23 @@ export function useDebounce<T>(value: T, delay = 500): T {
   }, [value, delay]);
 
   return debouncedValue;
+}
+
+export function useActiveId() {
+  const [activeId, setActiveId] = useState<number | null>(null);
+
+  useEffect(() => {
+    const handleHashChange = () => {
+      const id = +window.location.hash.slice(1);
+      setActiveId(id);
+    };
+    handleHashChange();
+
+    window.addEventListener('hashchange', handleHashChange);
+
+    return () => {
+      window.removeEventListener('hashchange', handleHashChange);
+    };
+  }, []);
+  return activeId;
 }
